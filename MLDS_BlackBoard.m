9e61169d0a4bcea251986be85c5e716b49182168
onatselim <onatselim@gmail.com>
2016-10-11 13:33:40 +0200

    new experimental lines

--- MLDS_BlackBoard.m
+++ MLDS_BlackBoard.m
@@ -40,3 +40,33 @@ for mu    = [0 1 2 3];
         %
     end
 end
+%% logistic with 2D a majestic try out
+
+%% Subject
+%get a subject with known 2D Psi values, where the distance increases with
+%a power law. Create also a 1D subject.
+Subject2D = MLDS_CreateSubject(2,'power');
+plot(Subject2D(1,:),Subject2D(2,:),'ro-');
+Subject1D = MLDS_CreateSubject(1,'power');
+plot(Subject1D(1,:),'ro-');
+%% get stimuli: 
+%Circularly organized 8 stimuli, repeat 1000 times so that we dont have issues on not enough data.
+StimList = MLDS_GetStimlist(2,1000,5);
+X        = MLDS_Q2DM(StimList);% a beautiful design matrix
+
+%% get responses
+R1D = MLDS_GetResponses(StimList,Subject1D,0.25)';%noise is Gaussian(0,0.25), we have to think whether this is a good noise model, coz responses should never because less than 0.
+R2D = MLDS_GetResponses(StimList,Subject2D,0.25)';
+
+%% use TK magic, first proof of concept
+% PsiValues = glmfit(X,R1D,'binomial','link','logit','constant','on');%this will not work because the rank of X is 7, however it has 8 columns.
+%here we remove one column from X, this means we set that psi to 0.
+PsiValues = glmfit(X(:,2:end),R1D,'binomial','link','logit','constant','on');%Constant=Off, gives crap results, think about it.
+%plot the results.
+clf;plot(zscore([0 ;PsiValues(2:end)]),'o');hold on;plot(zscore(Subject1D),'r-');hold off;%looks perfect.
+%% so now let's try the SO magic with 2D.
+PsiValues = glmfit(X(:,2:end),R2D,'binomial','link','logit','constant','off');%Constant=Off, gives crap results, think about it.
+clf;plot(zscore([0 ;PsiValues(2:end)]),'o');hold on;plot(zscore(Subject2D(1,:)),'r-');hold off;%looks like we get the 1 cycle of oscilation.
+%% now we would like to project distances in X to two orthognal spaces and re-run the same analysis...
+PsiValues = glmfit(X(:,2:end),R2D,'binomial','link','logit','constant','off');%Constant=Off, gives crap results, think about it.
+clf;plot(zscore([0 ;PsiValues(2:end)]),'o');hold on;plot(zscore(Subject2D(1,:)),'r-');hold off;%looks like we get the 1 cycle of oscilation.
